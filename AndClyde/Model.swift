c62f722a45aa57ae12a64a673c0c43707cc52349
Anton Ivanou <anivanov@scand.com>
2021-05-22 00:49:02 +0300

    Add test 3d models.  Setup multiuser experience with test model.

---
+++ AndClyde/Model.swift
@@ -0,0 +1,63 @@
+//
+//  Model.swift
+//  AndClyde
+//
+//  Created by Anton Ivanov on 21.05.21.
+//
+
+import RealityKit
+import UIKit
+
+class Model {
+    var name: String
+    var thumbnails: UIImage
+    var modelEntity: ModelEntity?
+    var scaleCompensation: Float
+        
+    init(_ name: String, scaleCompensation: Float = 1.0) {
+        self.name = name
+        self.thumbnails = UIImage(named: name) ?? UIImage(systemName: "photo")!
+        self.scaleCompensation = scaleCompensation
+    }
+    
+    func asyncLoadModelEntity() {
+        let filename = self.name + ".usdz"
+        
+        if let mEntity = try? ModelEntity.loadModel(named: filename) {
+            self.modelEntity = mEntity
+            self.modelEntity?.scale *= self.scaleCompensation
+            
+            print("modelEntity for \(self.name) has been loaded.")
+        } else {
+            print("Unable to load modelEntity for \(filename).")
+        }
+        
+//        _ = ModelEntity.loadModelAsync(named: filename)
+//            .sink(receiveCompletion: { loadCompletion in
+//                switch loadCompletion {
+//                case .failure(let error):
+//                case .finished:
+//                    break
+//                }
+//            }, receiveValue: { modelEntity in
+//                self.modelEntity = modelEntity
+//                self.modelEntity?.scale *= self.scaleCompensation
+//
+//                print("modelEntity for \(self.name) has been loaded.")
+//            })
+    }
+}
+
+struct Models {
+    var all: [Model] = []
+    
+    init() {
+        let gold = Model("stone", scaleCompensation: 1)
+        let shell = Model("shell", scaleCompensation: 0.5)
+        let wood = Model("wood", scaleCompensation: 0.5)
+        let chair = Model("chair", scaleCompensation: 0.1)
+
+        self.all += [gold, shell, wood, chair]
+
+    }
+}
--- AndClyde/ViewController.swift
+++ AndClyde/ViewController.swift
@@ -13,6 +13,11 @@ import MultipeerSession
 class ViewController: UIViewController {
     
     @IBOutlet var arView: FocusARView!
+    var selectedModel: Model? {
+        didSet {
+            arView.focusEntity?.isEnabled = self.selectedModel != nil
+        }
+    }
 
     private lazy var addModelButton: UIButton = {
         let button = UIButton()
@@ -25,7 +30,7 @@ class ViewController: UIViewController {
         return button
     }()
     
-    private lazy var connectionStatusView: UIView = {
+    lazy var connectionStatusView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = 5
         view.layer.backgroundColor = UIColor.systemRed.cgColor
@@ -117,22 +122,49 @@ class ViewController: UIViewController {
     }
     
     func placeObject(named entityName: String, for anchor: ARAnchor) {
-        let laserEntity = try! ModelEntity.load(named: entityName)
-        let anchorEntity = AnchorEntity(anchor: anchor)
-        anchorEntity.addChild(laserEntity)
-        arView.scene.addAnchor(anchorEntity)
+        let model = Model(entityName)
+        model.asyncLoadModelEntity()
+        
+        if let entity = model.modelEntity {
+            
+            let clonedEntity = entity.clone(recursive: true)
+            
+            clonedEntity.generateCollisionShapes(recursive: true)
+            arView.installGestures([.translation, .rotation, .scale], for: clonedEntity)
+            
+            let anchorEntity = AnchorEntity(anchor: anchor)
+            anchorEntity.addChild(clonedEntity)
+            arView.scene.addAnchor(anchorEntity)
+        }
     }
     
     @objc
     func addModelHandled(sender: UIButton) {
-        print("Hello world")
+                
+        let model = Models().all[0]
+        
+        selectedModel = model
+        selectedModel?.asyncLoadModelEntity()
+
+        let location = CGPoint(x: arView.bounds.maxX / 2, y: arView.bounds.maxY / 2)
+
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+
+
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: model.name, transform: firstResult.worldTransform)
+
+            arView.session.add(anchor: anchor)
+        } else {
+            print("Object placement failed - couldn't find surface.")
+        }
     }
 }
 
 extension ViewController: ARSessionDelegate {
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         for anchor in anchors {
-            if let anchorname = anchor.name, anchorname == "shell" {
+            if let anchorname = anchor.name {
                 placeObject(named: anchorname, for: anchor)
             }
             
@@ -140,120 +172,15 @@ extension ViewController: ARSessionDelegate {
                 print("Successfully connected to another user")
                 connectionStatusView.layer.backgroundColor = UIColor.systemGreen.cgColor
                 let anchorEntity = AnchorEntity(anchor: participantAnchor)
-                
+
                 let mesh = MeshResource.generateSphere(radius: 0.03)
                 let color = UIColor.red
                 let material = SimpleMaterial(color: color, isMetallic: false)
                 let coloredSphere = ModelEntity(mesh: mesh, materials: [material])
-                
+
                 anchorEntity.addChild(coloredSphere)
-                
+
                 arView.scene.addAnchor(anchorEntity)
-            }
-        }
-    }
-}
-
-// MARK: - Multipeer
-
-extension ViewController {
-    private func sendARSessionIDTo(peers: [PeerID]) {
-        guard let multipeerSession = multipeerSession else { return }
-        let idString = arView.session.identifier.uuidString
-        let command = "SessionID:" + idString
-        if let commandData = command.data(using: .utf8) {
-            multipeerSession.sendToPeers(commandData, reliably: true, peers: peers)
-        }
-    }
-    
-    func receivedData(_ data: Data, from peer: PeerID) {
-        guard let multipeerSession = multipeerSession else { return }
-        
-        if let collaborationData = try? NSKeyedUnarchiver.unarchivedObject(ofClass: ARSession.CollaborationData.self, from: data) {
-            arView.session.update(with: collaborationData)
-            return
-        }
-        
-        let sessionIDCommandString = "SessionID:"
-        if let commandString = String(data: data, encoding: .utf8), commandString.starts(with: sessionIDCommandString) {
-            let newSessionID = String(commandString[commandString.index(commandString.startIndex, offsetBy: sessionIDCommandString.count)...])
-            
-            // if this peer was using a different session ID before, remove all its assosiated anchors.
-            // This will remove old participant anchor and its geometru from the scene.
-            if let oldSessionID = multipeerSession.peerSessionIDs[peer] {
-                removeAllAnchorsOriginatingFromARSessionWithID(oldSessionID)
-            }
-            
-            multipeerSession.peerSessionIDs[peer] = newSessionID
-        }
-    }
-    
-    func peerDiscovered(_ peer: PeerID) -> Bool {
-        guard let multipeerSession = multipeerSession else { return false }
-        
-        if multipeerSession.connectedPeers.count > 4 {
-            // Do not accept more than four users in the experience.
-            print("A fifth peer wants to join the experience.\nThis app is limited to four users.")
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    /// - Tag: PeerJoined
-    func peerJoined(_ peer: PeerID) {
-        print("""
-            A peer wants to join the experience.
-            Hold the phones next to each other.
-            """)
-        
-        DispatchQueue.main.async {
-            self.connectionStatusView.layer.backgroundColor = UIColor.systemYellow.cgColor
-        }
-        
-        // Provide your session ID to the new user so they can keep track of your anchors.
-        sendARSessionIDTo(peers: [peer])
-    }
-    
-    func peerLeft(_ peer: PeerID) {
-        guard let multipeerSession = multipeerSession else { return }
-
-        print("A peer has left the shared experience.")
-        DispatchQueue.main.async {
-            self.connectionStatusView.layer.backgroundColor = UIColor.systemRed.cgColor
-        }
-
-        // Remove all ARAnchors associated with the peer that just left the experience.
-        if let sessionID = multipeerSession.peerSessionIDs[peer] {
-            removeAllAnchorsOriginatingFromARSessionWithID(sessionID)
-            multipeerSession.peerSessionIDs.removeValue(forKey: peer)
-        }
-    }
-    
-    private func removeAllAnchorsOriginatingFromARSessionWithID(_ identifier: String) {
-        guard let frame = arView.session.currentFrame else { return }
-        for anchor in frame.anchors {
-            guard let anchorSessionID = anchor.sessionIdentifier else { continue }
-            if anchorSessionID.uuidString == identifier {
-                arView.session.remove(anchor: anchor)
-            }
-        }
-    }
-    
-    /// - Tag: DidOutputCollaborationData
-    func session(_ session: ARSession, didOutputCollaborationData data: ARSession.CollaborationData) {
-        guard let multipeerSession = multipeerSession else { return }
-        if !multipeerSession.connectedPeers.isEmpty {
-            guard let encodedData = try? NSKeyedArchiver.archivedData(withRootObject: data, requiringSecureCoding: true)
-            else { fatalError("Unexpectedly failed to encode collaboration data.") }
-            // Use reliable mode if the data is critical, and unreliable mode if the data is optional.
-            let dataIsCritical = data.priority == .critical
-            multipeerSession.sendToAllPeers(encodedData, reliably: dataIsCritical)
-        } else {
-            print("Deferred sending collaboration to later because there are no peers.")
-            let color = connectionStatusView.layer.backgroundColor
-            UIView.animate(withDuration: 0.1) {
-                self.connectionStatusView.layer.backgroundColor = UIColor.systemBlue.cgColor
             }
         }
     }
